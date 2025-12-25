d24cf24322db3e9e3e3a3855b86493649f2a74f9
jad kobeissi <jadkoneissi@gmail.com>
2025-12-25 19:02:02 +0200

    feat: implement user update functionality; add UPDATE API route and handle user data updates with validation

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,6 +1,7 @@
 import { verify, decode } from "jsonwebtoken";
 import { prisma } from "../../init";
 import { TJWT } from "@/app/types";
+import { isEmpty } from "../../isEmpty";
 
 export async function GET(
   req: Request,
@@ -59,6 +60,43 @@ export async function DELETE(
     });
 
     return Response.json(user);
+  } catch (error: any) {
+    return new Response(error, { status: 500 });
+  }
+}
+export async function UPDATE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const authHeader = req.headers.get("Authorization")?.split(" ")[1];
+
+    if (!authHeader || !verify(authHeader, process.env.JWT_SECRET as string))
+      return new Response("Unauthorized", { status: 401 });
+
+    const decoded = (await decode(authHeader)) as TJWT;
+
+    const { id } = await params;
+
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (!user) return new Response("User not found", { status: 404 });
+
+    if (decoded.id !== user.id)
+      return new Response("Unauthorized", { status: 401 });
+    const { username, email } = await req.json();
+
+    await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        username: username || username !== "" ? username : user.username,
+        email: email || email !== "" ? email : user.email,
+      },
+    });
+
+    return new Response("User updated");
   } catch (error: any) {
     return new Response(error, { status: 500 });
   }
